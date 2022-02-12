efcdc60909f0264b715820834e70f7bc336f9296
Alexander Kaplan <kaplan2539@gmail.com>
2022-02-12 21:51:28 +0100

    chg label

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='hello kivy')
+        return Label(text='Hello Kivy')
 
 if __name__ == '__main__':
     MyApp().run()
